3021b2def23d222e789494962220dde3cfaef865
Ahmed-Alabadla <eng.ahmedalabadla@gmail.com>
2025-10-04 10:57:41 +0300

    feat: Update README with new repository link and implement admin login page with validation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,42 +1,35 @@
 import { createBrowserRouter } from "react-router-dom";
-import {
-  LandingPage,
-  Login,
-  Signup,
-  Therapists,
-  TherapistPage,
-  LoginAdmin,
-  NotFound,
-} from "../pages";
+
 import Layout from "../layout/Layout";
 import { ProtectedAdmin, ProtectedUser } from "./protected";
-import AdminTherapists from "../pages/adminTherapists";
-import BugReportPage from "../pages/bugsReport";
-import AdminBugs from "../pages/adminBugs";
+import Signup from "../pages/signup";
+import Login from "../pages/login";
+import NotFound from "../pages/notFound";
+import LoginAdmin from "../pages/loginAdmin";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <LandingPage />,
-      },
-      {
-        path: "therapists",
-        element: <Therapists />,
-      },
-      {
-        path: "therapist/:id",
-        element: <TherapistPage />,
-      },
-      {
-        path: "bug-report",
-        element: <BugReportPage />,
-      },
-    ],
-  },
+  // {
+  //   path: "/",
+  //   element: <Layout />,
+  //   children: [
+  //     {
+  //       path: "",
+  //       element: <LandingPage />,
+  //     },
+  //     {
+  //       path: "therapists",
+  //       element: <Therapists />,
+  //     },
+  //     {
+  //       path: "therapist/:id",
+  //       element: <TherapistPage />,
+  //     },
+  //     {
+  //       path: "bug-report",
+  //       element: <BugReportPage />,
+  //     },
+  //   ],
+  // },
   {
     path: "signup",
     element: (
@@ -57,20 +50,20 @@ const router = createBrowserRouter([
     path: "admin/login",
     element: <LoginAdmin />,
   },
-  {
-    path: "admin/therapists",
-    element: <ProtectedAdmin />,
-    children: [
-      {
-        path: "",
-        element: <AdminTherapists />,
-      },
-      {
-        path: "bugs",
-        element: <AdminBugs />,
-      },
-    ],
-  },
+  // {
+  //   path: "admin/therapists",
+  //   element: <ProtectedAdmin />,
+  //   children: [
+  //     {
+  //       path: "",
+  //       element: <AdminTherapists />,
+  //     },
+  //     {
+  //       path: "bugs",
+  //       element: <AdminBugs />,
+  //     },
+  //   ],
+  // },
   { path: "*", element: <NotFound /> },
 ]);
 
