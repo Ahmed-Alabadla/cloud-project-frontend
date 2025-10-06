22babc9064bf4ba27941a0d41225216b9faaee51
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-10-06 14:08:27 +0300

    feat: Add TypeScript interface for textFieldWithEdited component props

---
+++ src/components/TherapistProfile/textFieldWithEdited/types.ts
@@ -0,0 +1,8 @@
+export interface Props {
+  value: string | number | undefined;
+  dataType: "fullName" | "hourlyRate" | "major";
+  // eslint-disable-next-line no-unused-vars
+  onChange: (value: string | number) => void;
+  isProfileOwner: boolean;
+  themeMode: string;
+}
