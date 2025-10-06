fe34389d0e4d496ab020fdf2ad708efa4cd243db
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-10-06 14:08:37 +0300

    feat: Add getStyle function for dynamic styling of textFieldWithEdited component

---
+++ src/components/TherapistProfile/textFieldWithEdited/style.ts
@@ -0,0 +1,18 @@
+const getStyle = (dataType: string, themeMode: string) => ({
+  width: dataType === "hourlyRate" ? "335px" : "450px",
+  margin: "0.5px ", // Adds a small margin around the input
+  "& .MuiInputBase-input.Mui-disabled": {
+    WebkitTextFillColor: themeMode === "dark" ? "#eee" : "black", //#2B127B
+    height: "17px",
+    marginTop: dataType === "hourlyRate" ? "-5px" : "30px",
+  },
+  "& input": {
+    color: "red",
+    fontSize: dataType === "fullName" ? "24px" : "18px",
+    fontWeight: dataType === "fullName" ? "bolder" : "bold",
+    marginTop: dataType === "hourlyRate" ? "-5px" : "30px",
+    height: "17px",
+  },
+});
+
+export default getStyle;
